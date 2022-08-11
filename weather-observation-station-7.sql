f2eb0081e69d2d137ebdc1d43549c1fc17652953
ian <38280903+iancarson@users.noreply.github.com>
2022-08-11 21:15:18 +0300

    Create weather-observation-station-7.sql

---
+++ weather-observation-station-7.sql
@@ -0,0 +1,4 @@
+/*
+Enter your query here.
+*/
+SELECT DISTINCT CITY FROM STATION WHERE RIGHT(CITY,1) IN ('a','i','e','o','u');                                                                          
